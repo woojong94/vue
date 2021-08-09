9067bdf2b2f1c65f18e668876ddeba7da5458d7e
woojong94 <woodong9412@gmail.com>
2021-08-09 18:32:13 +0900

    vue0809

---
+++ vue-project/src/views/School.vue
@@ -0,0 +1,13 @@
+<template>
+    <h1> School.vue </h1>
+    <h2>{{ title }}</h2>
+</template>
+<script>
+export default {
+    data() {
+        return {
+            title : "제목",
+        };
+    }
+}
+</script>
---
+++ vue-project2/src/views/School.vue
@@ -0,0 +1,31 @@
+<template>
+    <h1>School.vue</h1>
+    <h2 @click="changeTitle">{{ title }}</h2>
+    <SchoolTitle />
+    <school-title />
+</template>
+<script>
+import SchoolTitle from "../components/SchoolTitle.vue"
+export default {
+    components : {SchoolTitle},
+    data() {
+        return {
+            title : "학교 홈페이지",
+        }
+    },
+    created() {
+
+    },
+    mounted() {
+
+    },
+    unmounted() {
+
+    },
+    methods : {
+        changeTitle() {
+            this.title = "(변경) 학교 홈페이지";
+        }
+    }
+}
+</script>
